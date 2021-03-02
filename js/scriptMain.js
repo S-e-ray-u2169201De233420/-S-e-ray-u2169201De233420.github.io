93f2c84dd8540a81757a48d7e620654c9d4fb179
Chiesa Afandy <user.email>
2021-03-02 23:33:50 +0700

    commit animepages Video Screenshot

---
+++ js/scriptMain.js
@@ -0,0 +1,26 @@
+function spoiler_desc() {
+    var x = document.getElementById("spoiler_desc");
+    if (x.innerHTML === "See Less..") {
+      x.innerHTML = "See More..";
+    } else {
+      x.innerHTML = "See Less..";
+    }
+  }
+
+  function spoiler_video() {
+    var x = document.getElementById("spoiler_video");
+    if (x.innerHTML === "See Less..") {
+      x.innerHTML = "See More..";
+    } else {
+      x.innerHTML = "See Less..";
+    }
+  }
+
+  function spoiler_link() {
+    var x = document.getElementById("spoiler_link");
+    if (x.innerHTML === "See Less..") {
+      x.innerHTML = "See More..";
+    } else {
+      x.innerHTML = "See Less..";
+    }
+  }
