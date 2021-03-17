3fdd0bf9d3d145a7f852a8274bc76bf7aa84033e
Chiesa Afandy <user.email>
2021-03-17 22:32:44 +0700

    commit ReWork Design - fix javascript - fix video

--- js/scriptMain.js
+++ js/scriptMain.js
@@ -1,38 +1,60 @@
 function spoiler_desc() {
-    var x = document.getElementById("spoiler_desc");
-    if (x.innerHTML === "See Less..") {
-      x.innerHTML = "See More..";
-    } else {
-      x.innerHTML = "See Less..";
-    }
+  var x = document.getElementById("spoiler_desc");
+  if (x.innerHTML === "See Less..") {
+    x.innerHTML = "See More..";
+  } else {
+    x.innerHTML = "See Less..";
   }
+}
 
-  function spoiler_video() {
-    var x = document.getElementById("spoiler_video");
-    if (x.innerHTML === "See Less..") {
-      x.innerHTML = "See More..";
-    } else {
-      x.innerHTML = "See Less..";
-    }
+function spoiler_video() {
+  var x = document.getElementById("spoiler_video");
+  if (x.innerHTML === "See Less..") {
+    x.innerHTML = "See More..";
+  } else {
+    x.innerHTML = "See Less..";
   }
+}
 
-  function spoiler_link() {
-    var x = document.getElementById("spoiler_link");
-    if (x.innerHTML === "See Less..") {
-      x.innerHTML = "See More..";
-    } else {
-      x.innerHTML = "See Less..";
-    }
+function spoiler_link() {
+  var x = document.getElementById("spoiler_link");
+  if (x.innerHTML === "See Less..") {
+    x.innerHTML = "See More..";
+  } else {
+    x.innerHTML = "See Less..";
   }
+}
 
-  function BlockedTest() {
-    alert("STOP RIGHT THERE \nini website design, beberapa fitur ga fungsional");
-  }
+function BlockedTest() {
+  alert("STOP RIGHT THERE \nini website design, beberapa fitur ga fungsional");
+}
 
-  function BlockedMagerTest() {
-    alert("FrontEnd nya lagi mager \nbelum ada fiturnya. mintanya nanti saja");
-  }
+function BlockedMagerTest() {
+  alert("FrontEnd nya lagi mager \nbelum ada fiturnya. mintanya nanti saja");
+}
 
-  function BlockedNotifyMe() {
-    alert("INI CONTOH NOTIFY ME \nbelum ada fiturnya. pokoknya nanti muncul kayak bacaan suruh allow gitu, urusan backend im not gonna touch this sh*t -frontend");
-  }
+function BlockedNotifyMe() {
+  alert("INI CONTOH NOTIFY ME \nbelum ada fiturnya. pokoknya nanti muncul kayak bacaan suruh allow gitu, urusan backend im not gonna touch this sh*t -frontend");
+}
+
+var vid = document.getElementById("myVideo");
+var vid2 = document.getElementById("myVideo1");
+
+function playVid() {
+  vid.autoplay = true;
+  vid.load();
+}
+
+function playVid2() {
+  vid2.autoplay = true;
+  vid2.load();
+  vid2.muted = true;
+}
+
+function stopvid() {
+  vid.pause();
+}
+
+function stopvid2() {
+  vid2.pause();
+}
